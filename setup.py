7e5277211fdae1fa4b245ef2d043ca5b6817d769
dohlee <dohlee.bioinfo@gmail.com>
2023-05-24 16:23:21 +0900

    1.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="chromoformer",
     packages=find_packages(exclude=[]),
     include_package_data=True,
-    version="1.1.1",
+    version="1.1.2",
     license="MIT",
     description="Chromoformer - Pytorch",
     author="Dohoon Lee",
